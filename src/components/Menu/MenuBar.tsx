c420eb122b52bff5c065490266332f2557736e1e
Oğuz Utku Yıldız <oguzutku1745@gmail.com>
2024-02-24 21:58:49 +0300

    Spelling

--- src/components/Menu/MenuBar.tsx
+++ src/components/Menu/MenuBar.tsx
@@ -72,7 +72,7 @@ const MegaMenu = () => {
             onChange={handlePrivateKeyChange}
           />
          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 whitespace-nowrap" onClick={setKey}>
-            {keyStat ? 'Setted' : 'Provide Key'}
+            {keyStat ? 'Settled' : 'Provide Key'}
           </button>
         </div>
         <ul className="flex">
